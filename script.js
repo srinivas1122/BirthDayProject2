c6cbe213dc7679fbf6b7bf2a762fb39fa21491de
KANDUKURI Srinivas <srinivas.kandukuri@hexagon.com>
2024-02-08 22:51:17 +0530

    Added about me page

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Animation for the ring to rotate
+//     const ring = document.querySelector(".ring-box ");
+//     ring.style.transition = "transform 2s ease-in-out";
+//     setInterval(() => {
+//       ring.style.transform = "rotate(5deg)";
+//       setTimeout(() => {
+//         ring.style.transform = "rotate(-5deg)";
+//       }, 1000);
+//     }, 2000);
+  
+//     // Animation for the "Will You Marry Me?" text to pulse
+//     const title = document.querySelector("");
+//     setInterval(() => {
+//       title.style.fontSize = "50px";
+//       setTimeout(() => {
+//         title.style.fontSize = "60px";
+//       }, 500);
+//     }, 1000);
+//   });
+  
